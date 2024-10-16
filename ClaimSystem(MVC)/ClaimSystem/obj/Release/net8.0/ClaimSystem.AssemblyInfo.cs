8e583be3e491248ba759caf6dccdf1a5e52ea255
KeanuM10 <st10306640@imconnect.edu.za>
2024-10-16 12:06:51 +0200

    Implemented Unit Tests
    Implemented 8 unit tests for HomeController.

--- ClaimSystem(MVC)/ClaimSystem/obj/Release/net8.0/ClaimSystem.AssemblyInfo.cs
+++ ClaimSystem(MVC)/ClaimSystem/obj/Release/net8.0/ClaimSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaimSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc59473c96c1a56cb9098ad07cb7694a51932c7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc537cb35f286e649a5cced2686757384490319")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaimSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaimSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
