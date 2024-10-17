0245e98aac19a09e0d523c890e8db9c5b481f1e6
KeanuM10 <st10306640@imconnect.edu.za>
2024-10-17 16:39:42 +0200

    Added a few extra comments just for clarity.
    A few extra comments have been added.

--- ClaimSystem(MVC)/ClaimSystem/obj/Debug/net8.0/ClaimSystem.AssemblyInfo.cs
+++ ClaimSystem(MVC)/ClaimSystem/obj/Debug/net8.0/ClaimSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaimSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00b2a1bf57a2f1be00573816826e42e157412fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23e24f0ce721ac8d91ee1b868b5c210d859c4e59")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaimSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaimSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
